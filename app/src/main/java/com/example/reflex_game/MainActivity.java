ba413d51f4290fcf163cc51a1a6f7b48337a7c03
Bence <boczekbence@gmail.com>
2022-04-06 16:59:12 +0200

    first steps in main activity gui

---
+++ app/src/main/java/com/example/reflex_game/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.reflex_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+
+    public void play(View view) {
+    }
+
+    public void register(View view) {
+    }
+}
