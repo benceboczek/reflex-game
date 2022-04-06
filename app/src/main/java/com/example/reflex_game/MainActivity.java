ba0ccb00c9e4b8cb3e2c976ced4eb187d00fd7c7
Bence <boczekbence@gmail.com>
2022-04-06 23:08:54 +0200

    firebase installed

--- app/src/main/java/com/example/reflex_game/MainActivity.java
+++ app/src/main/java/com/example/reflex_game/MainActivity.java
@@ -3,23 +3,39 @@ package com.example.reflex_game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+
     }
 
     public void play(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 
     public void register(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void anonymRegister(View view) {
+
     }
 }
